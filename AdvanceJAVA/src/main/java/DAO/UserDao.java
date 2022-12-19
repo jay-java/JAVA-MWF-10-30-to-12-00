74989ba40512759c4d61dadb3bfe3b9fb9f013f4
jay-java <jayprajapati.tops@gmail.com>
2022-12-19 12:01:31 +0530

    advance register

---
+++ AdvanceJAVA/src/main/java/DAO/UserDao.java
@@ -0,0 +1,26 @@
+package DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import Connection.DBConnection;
+import Model.User;
+
+public class UserDao {
+	public static void insetUser(User u) {
+		try {
+			Connection conn = DBConnection.createConnection();
+			String sql="insert into user(name,contact,address,email,password) values(?,?,?,?,?)";
+			PreparedStatement pst = conn.prepareStatement(sql);
+			pst.setString(1, u.getName());
+			pst.setLong(2, u.getContact());
+			pst.setString(3, u.getAddress());
+			pst.setString(4,u.getEmail());
+			pst.setString(5, u.getPassword());
+			pst.executeUpdate();
+			System.out.println("data inserted");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
